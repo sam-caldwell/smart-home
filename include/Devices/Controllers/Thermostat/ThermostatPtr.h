51d9957fb4a98c74fbc6f9dbc5730a60c0f5b9d7
Sam Caldwell <samcaldwell@mbp.local>
2025-04-16 10:16:11 -0500

    debugging issue with ThermostatPtr

--- include/Devices/Controllers/Thermostat/ThermostatPtr.h
+++ include/Devices/Controllers/Thermostat/ThermostatPtr.h
@@ -2,15 +2,9 @@
 // Author: Sam Caldwell <scaldwell@asymmetric-effort.com>
 // Description: Define the two modes of a thermostat (heat|cool)
 
-#ifndef THERMOSTAT_PTR_H
-#define THERMOSTAT_PTR_H
-
 #pragma once
-
 #include <memory>
 
-class Thermostat;  // Forward declare — no full class needed for unique_ptr
+class Thermostat;  // Forward declaration (global namespace)
 
 using ThermostatPtr = std::unique_ptr<Thermostat>;
-
-#endif // THERMOSTAT_PTR_H
