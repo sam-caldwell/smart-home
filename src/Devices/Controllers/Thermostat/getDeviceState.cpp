43ef56fab1b2fcc3bb2877ca9b77431cc3c39dfa
Sam Caldwell <samcaldwell@mbp.local>
2025-04-19 00:54:31 -0500

    Thermostat now updates from HTTP API

--- src/Devices/Controllers/Thermostat/getDeviceState.cpp
+++ src/Devices/Controllers/Thermostat/getDeviceState.cpp
@@ -12,9 +12,21 @@ ParserResult Thermostat::getDeviceState(){
 
         SimpleJson parser(json);
 
-        fanState.set(parser.getString("fan"));
-        modeState = to_thermostat_mode(parser.getString("mode"));
-        int temp = parser.getInt("temp");
+        {
+            const std::string rawFan=parser.getString("fan");
+            this->fanState.set(rawFan);
+            log->info("fanState: " + rawFan+ " stored as " + this->fanState.string());
+        }
+        {
+            const std::string rawMode = parser.getString("mode");
+           this->modeState = to_thermostat_mode(rawMode);
+            log->info("modeState: " + rawMode + " stored as " + to_string(this->modeState));
+        }
+        {
+            const std::string rawTemp=parser.getString("temp");
+            this->temperature = parser.getInt("temp");
+            log->info("temp " + rawTemp + " stored as " + std::to_string(this->temperature));
+        }
 
     }catch(...){
         log->error("Thermostat device did not respond properly. Try again later.");
