0327841962c4385f0c1a00f0c2c8846c32bcc54e
Sam Caldwell <samcaldwell@mbp.local>
2025-04-20 18:11:38 -0500

    fix typo

--- src/Devices/Controllers/Thermostat/getDeviceState.cpp
+++ src/Devices/Controllers/Thermostat/getDeviceState.cpp
@@ -1,51 +1,10 @@
-// Thermostat/getDeviceState.cpp
-// Author: Sam Caldwell <scaldwell@asymmetric-effort.com>
-// Description: Thermostat Http API device class
-
-#include "Devices/Controllers/Thermostat/Thermostat.h"
-
-ParserResult Thermostat::g// Thermostat/getRemoteState.cpp
+// Thermostat/getRemoteState.cpp
 // Author: Sam Caldwell <scaldwell@asymmetric-effort.com>
 // Description: Thermostat Http API device class
 
 #include "Devices/Controllers/Thermostat/Thermostat.h"
 
 ParserResult Thermostat::getDeviceState(){
-    log->info("Thermostat::getDeviceState()");
-    try{
-        const std::string json=getRemoteState();
-        log->info("state: " + json);
-
-        SimpleJson parser(json);
-
-        {
-            log->info("parsing fan state");
-            const std::string rawFan=parser.getString("fan");
-            this->fanState.set(rawFan);
-            log->info("fanState: " + rawFan+ " stored as " + this->fanState.string());
-        }
-        {
-           log->info("parsing mode");
-           const std::string rawMode = parser.getString("mode");
-           log->info("rawMode: " + rawMode);
-           this->modeState = to_thermostat_mode(rawMode);
-           log->info("modeState: " + rawMode + " stored as " + to_string(this->modeState));
-        }
-        {
-            log->info("parsing temperature");
-            const std::string rawTemp=parser.getString("temp");
-            this->temperature = parser.getInt("temp");
-            log->info("temp '" + rawTemp + "' stored as '" + std::to_string(this->temperature)+"'");
-        }
-
-    }catch(...){
-        log->error("Thermostat device did not respond properly. Try again later.");
-        std::cout << "Error: Cannot talk to thermostat." << std::endl;
-        return ParserResult::error;
-    }
-    return ParserResult::ok;
-}
-etDeviceState(){
     log->info("Thermostat::getDeviceState()");
     try{
         const std::string json=getRemoteState();
