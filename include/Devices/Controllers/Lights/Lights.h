4ea3de93842246ea395d8fdc2e8cda3d8a52d6e6
Sam Caldwell <github@samcaldwell.net>
2025-04-20 20:59:47 -0500

    add the Lights class

---
+++ include/Devices/Controllers/Lights/Lights.h
@@ -0,0 +1,33 @@
+// Security.h
+// Author: Sam Caldwell <scaldwell@asymmetric-effort.com>
+// Description: Security Http API device class
+
+#ifndef LIGHTS_DEVICE_H
+#define LIGHTS_DEVICE_H
+#include <sstream>
+#include <unordered_map>
+#include "utils/OnOff.h"
+#include "utils/StringUtils.h"
+#include "utils/SimpleJson/SimpleJson.h"
+#include "Logger/Logger.h"
+#include "ParserResult/ParserResult.h"
+#include "Devices/WebEnabledDevice/WebEnabledDevice.h"
+
+class Lights: public WebEnabledDevice {
+public:
+    explicit Lights(Logger* log, const std::string& connstr);
+    ~Lights() override;
+    ParserResult sendCommand(std::vector<std::string> &args) override;
+private:
+    inline void turnOn(const std::string &room);
+    inline void turnOff(const std::string &room);
+
+    ParserResult getDeviceState();
+    ParserResult updateDeviceState();
+
+    using LightMap = std::unordered_map<std::string, OnOff>;
+
+    LightMap *lights;
+};
+
+#endif //LIGHTS_DEVICE_H
