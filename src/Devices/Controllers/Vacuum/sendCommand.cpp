e8851f840bd4cc24929a1591942685667ae85262
Sam Caldwell <github@samcaldwell.net>
2025-04-22 10:34:27 -0500

    queue-based parse/routing is feature complete

--- src/Devices/Controllers/Vacuum/sendCommand.cpp
+++ src/Devices/Controllers/Vacuum/sendCommand.cpp
@@ -6,35 +6,22 @@
 
 // receive command-line input for processing.
 ParserResult Vacuum::sendCommand(Tokens &tokens) {
-
     log->info("Lights::sendCommand start");
-
-    int argc = args.size();
-
-    const std::string subCommand = to_lower(args[1]); //set|get
 
     //Update internal state from the device before we do anything. Bail on error
     if (this->getDeviceState() != ParserResult::ok)
         return ParserResult::ok; //Bail.  We've already said something to the user.  swallow the error
 
-    if (argc <2) {
-        log->error("Television::sendCommand: wrong number of arguments");
-        return ParserResult::missingArgument;
-    }
-    if (subCommand == "get"){
+    if (const std::string subCommand = tokens.pop(); subCommand == "get") {
         std::cout << "running: " << runState.string() << std::endl;
         return ParserResult::ok;
     } else if (subCommand == "on") {
         runState.on();
-        return updateDeviceState();
-    }else if (subCommand == "off") {
+    } else if (subCommand == "off") {
         runState.off();
-        return updateDeviceState();
-    }else{
+    } else {
         std::cout << "unknown command: " << subCommand << std::endl;
         return ParserResult::missingArgument;
     }
-    std::cout << "I don't know what you want to do. I'm pretty sure that's illegal in this county." << std::endl;
-    return ParserResult::badCommand;
+    return updateDeviceState();
 }
-
