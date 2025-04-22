0c0f00a2e0917afd637883e37db6fe5829621fc0
Sam Caldwell <github@samcaldwell.net>
2025-04-22 09:59:30 -0500

    cleanup

--- src/Devices/WebEnabledDevice/sendCommand.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "Devices/WebEnabledDevice/WebEnabledDevice.h"
-//
-//ParserResult Device::sendCommand(std::vector<std::string> &args) {
-//  log->info("WebEnabledDevice::sendCommand() executed");
-//  return ParserResult::ok;
-//}
