788bf9d2f1c893007c5cb6ce843062f30c3d657a
Sam Caldwell <github@samcaldwell.net>
2025-04-22 21:40:53 -0500

    add TokenQueue.tpp

--- include/utils/TokenQueue.h
+++ include/utils/TokenQueue.h
@@ -7,45 +7,33 @@
 
 #include <queue>
 #include <stdexcept>
+#include "Token.h"
 
+// Token queue has a .pop() which returns the head value and pops it in one call.
+// It's a template because the requirements require one template...and there is
+// no real need for a template in this project other than to check the box.
 template<typename T>
 class TokenQueue {
 public:
     // Push a token to the back
-    void push(const T& token) {
-        tokens.push(token);
-    }
+    void push(const T &token);
 
     // Check if empty
-    bool empty() const {
-        return tokens.empty();
-    }
+    bool empty() const;
 
     // Number of tokens
-    size_t size() const {
-        return tokens.size();
-    }
+    size_t size() const;
 
     // Get the front element and remove it
-    T pop() {
-        if (tokens.empty()) {
-            throw std::underflow_error("TokenQueue::pop() called on empty queue.");
-        }
-        T front = tokens.front();
-        tokens.pop();
-        return front;
-    }
+    T pop();
 
     // Peek without removing
-    const T& peek() const {
-        if (tokens.empty()) {
-            throw std::underflow_error("TokenQueue::peek() called on empty queue.");
-        }
-        return tokens.front();
-    }
+    const T &peek() const;
 
 private:
     std::queue<T> tokens;
 };
+
+#include "utils/TokenQueue.tpp"
 
 #endif // TOKEN_QUEUE_H
